a31561a3aaa869fa352c8b272bc87dae7bff9845
Jonathan Hooker <Jonathan@oblio.io>
2024-01-30 15:26:55 -0800

    add books to homepage

--- components/Header.js
+++
@@ -1,8 +0,0 @@
-import styles from './Header.module.css'
-
-export default function Header({ title }) {
-  return <header  className={styles.header}>
-    <img src="/logo.svg" alt="Strange Goose Logo" className={styles.logo} />
-    {title}
-  </header>
-}
